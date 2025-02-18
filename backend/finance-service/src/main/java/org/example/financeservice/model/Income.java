0c9c4ea246d134bd975dd201c6dd8bc929e42c84
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-02-18 20:19:46 -0300

    feat: add income model

---
+++ backend/finance-service/src/main/java/org/example/financeservice/model/Income.java
@@ -0,0 +1,26 @@
+package org.example.financeservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Income {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private BigDecimal incomeAmount;
+    private LocalDate incomeDate;
+    private String incomeDescription;
+}
