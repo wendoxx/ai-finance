c8f54167e34b51d50d26c3da228b28480dd42126
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-02-18 20:44:03 -0300

    feat: add repositories

---
+++ backend/finance-service/src/main/java/org/example/financeservice/repository/ExpenseRepository.java
@@ -0,0 +1,11 @@
+package org.example.financeservice.repository;
+
+import org.example.financeservice.model.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, UUID> {
+}
