c72b4ec21d7a0910a96c92f99550076d0100e4e6
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-02-21 20:55:39 -0300

    feat: add methot to get expense by id

--- backend/finance-service/src/main/java/org/example/financeservice/service/ExpenseService.java
+++ backend/finance-service/src/main/java/org/example/financeservice/service/ExpenseService.java
@@ -1,4 +1,20 @@
 package org.example.financeservice.service;
 
+import org.example.financeservice.dto.response.ExpenseResponseDTO;
+import org.example.financeservice.repository.ExpenseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.function.Supplier;
+
+@Service
 public class ExpenseService {
+
+    private ExpenseRepository expenseRepository;
+
+    public ExpenseResponseDTO getExpenseById(UUID id) {
+        return expenseRepository.findById(id).map(ExpenseResponseDTO::new)
+                .orElseThrow(() -> new RuntimeException("Expense not found"));
+    }
+    
 }
