eeace0a09f71e85bcf1af628c375e8b5ff1bb370
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-02-21 21:07:48 -0300

    feat: add method to create, update and delete an expense

--- backend/finance-service/src/main/java/org/example/financeservice/dto/request/ExpenseRequestDTO.java
+++ backend/finance-service/src/main/java/org/example/financeservice/dto/request/ExpenseRequestDTO.java
@@ -1,6 +1,7 @@
 package org.example.financeservice.dto.request;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
-public record ExpenseRequestDTO(UUID id, String amount, String date, String description) {
+public record ExpenseRequestDTO(UUID id, BigDecimal amount, String date, String description) {
 }
--- backend/finance-service/src/main/java/org/example/financeservice/service/ExpenseService.java
+++ backend/finance-service/src/main/java/org/example/financeservice/service/ExpenseService.java
@@ -1,11 +1,12 @@
 package org.example.financeservice.service;
 
+import org.example.financeservice.dto.request.ExpenseRequestDTO;
 import org.example.financeservice.dto.response.ExpenseResponseDTO;
+import org.example.financeservice.model.Expense;
 import org.example.financeservice.repository.ExpenseRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
-import java.util.function.Supplier;
 
 @Service
 public class ExpenseService {
@@ -16,5 +17,23 @@ public class ExpenseService {
         return expenseRepository.findById(id).map(ExpenseResponseDTO::new)
                 .orElseThrow(() -> new RuntimeException("Expense not found"));
     }
-    
+
+    public Expense createAndUpdateExpense(ExpenseRequestDTO expenseRequestDTO) {
+        Expense expense;
+        if(expenseRequestDTO.id() != null && expenseRepository.existsById(expenseRequestDTO.id())) {
+            expense = expenseRepository.findById(expenseRequestDTO.id()).get();
+        } else {
+            expense = new Expense();
+        }
+
+        expense.setAmount(expenseRequestDTO.amount());
+        expense.setDate(expenseRequestDTO.date());
+        expense.setDescription(expenseRequestDTO.description());
+
+        return expenseRepository.save(expense);
+    }
+
+    public void deleteExpense(UUID id) {
+        expenseRepository.deleteById(id);
+    }
 }
