5064a9e2f8ecd0227259fb35896e1962c97d3060
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-02-21 21:20:04 -0300

    feat: add crud methods in income service

--- backend/finance-service/src/main/java/org/example/financeservice/service/IncomeService.java
+++ backend/finance-service/src/main/java/org/example/financeservice/service/IncomeService.java
@@ -1,4 +1,41 @@
 package org.example.financeservice.service;
 
+import org.example.financeservice.dto.request.IncomeRequestDTO;
+import org.example.financeservice.dto.response.IncomeResponseDTO;
+import org.example.financeservice.model.Income;
+import org.example.financeservice.repository.IncomeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class IncomeService {
+
+    @Autowired
+    private IncomeRepository incomeRepository;
+
+    public IncomeResponseDTO getIncomeById(UUID id) {
+        return incomeRepository.findById(id).map(IncomeResponseDTO::new)
+                .orElseThrow(() -> new RuntimeException("Income not found"));
+    }
+
+    public Income createAndUpdateIncome(IncomeRequestDTO incomeRequestDTO) {
+        Income income;
+        if(incomeRequestDTO.id() != null && incomeRepository.existsById(incomeRequestDTO.id())) {
+            income = incomeRepository.findById(incomeRequestDTO.id()).get();
+        } else {
+            income = new Income();
+        }
+
+        income.setAmount(incomeRequestDTO.amount());
+        income.setDate(incomeRequestDTO.date());
+        income.setDescription(incomeRequestDTO.incomeDescription());
+
+        return incomeRepository.save(income);
+    }
+
+    public void deleteIncome(UUID id) {
+        incomeRepository.deleteById(id);
+    }
 }
