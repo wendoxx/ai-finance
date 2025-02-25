ec46a590a1c077230ff59700ae51ee79693744cb
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-02-25 18:36:58 -0300

    feat: add initial security logic

--- backend/finance-service/src/main/java/org/example/financeservice/dto/request/IncomeRequestDTO.java
+++ backend/finance-service/src/main/java/org/example/financeservice/dto/request/IncomeRequestDTO.java
@@ -4,5 +4,5 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record IncomeRequestDTO(UUID id, BigDecimal amount, LocalDate date, String incomeDescription) {
+public record IncomeRequestDTO(UUID id, BigDecimal amount, LocalDate date, String description) {
 }
