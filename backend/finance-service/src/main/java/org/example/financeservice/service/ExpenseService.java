9d52ebad7ad0bd18e2c475b9cb4957023fc66ca2
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-02-25 17:38:16 -0300

    feat: add @Autowired annotation in expense service

--- backend/finance-service/src/main/java/org/example/financeservice/service/ExpenseService.java
+++ backend/finance-service/src/main/java/org/example/financeservice/service/ExpenseService.java
@@ -4,6 +4,7 @@ import org.example.financeservice.dto.request.ExpenseRequestDTO;
 import org.example.financeservice.dto.response.ExpenseResponseDTO;
 import org.example.financeservice.model.Expense;
 import org.example.financeservice.repository.ExpenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -11,6 +12,7 @@ import java.util.UUID;
 @Service
 public class ExpenseService {
 
+    @Autowired
     private ExpenseRepository expenseRepository;
 
     public ExpenseResponseDTO getExpenseById(UUID id) {
