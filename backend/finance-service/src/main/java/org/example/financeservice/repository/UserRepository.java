0683d201b01ba4c00f9923314f408f978bded39c
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-02-28 19:49:50 -0300

    feat: add user repository

---
+++ backend/finance-service/src/main/java/org/example/financeservice/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.example.financeservice.repository;
+
+import org.example.financeservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    UserDetails loadUserByUsername(String username);
+}
