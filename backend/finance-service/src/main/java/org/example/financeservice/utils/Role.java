de047b71c424c58f1510e15737fa72062838801d
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-02-28 19:02:23 -0300

    feat: add roles

---
+++ backend/finance-service/src/main/java/org/example/financeservice/utils/Role.java
@@ -0,0 +1,16 @@
+package org.example.financeservice.utils;
+
+public enum Role {
+    USER("user"),
+    ADMIN("admin");
+
+    private String role;
+
+    Role(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
