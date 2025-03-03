927018cde0f49f7278dfda667b2177b6f75cab34
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-03-03 18:39:49 -0300

    fix: correct user repository

--- backend/finance-service/src/main/java/org/example/financeservice/repository/UserRepository.java
+++ backend/finance-service/src/main/java/org/example/financeservice/repository/UserRepository.java
@@ -2,12 +2,11 @@ package org.example.financeservice.repository;
 
 import org.example.financeservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    UserDetails loadUserByUsername(String username);
+    User findByEmail(String email);
 }
