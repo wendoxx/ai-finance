c9b7a8aa673a7c272fb5708890b12cb41cc617da
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-03-06 15:46:47 -0300

    refactor: refacor dtos

--- backend/finance-service/src/main/java/org/example/financeservice/dto/request/RegisterRequestDTO.java
+++ backend/finance-service/src/main/java/org/example/financeservice/dto/request/RegisterRequestDTO.java
@@ -1,6 +1,7 @@
 package org.example.financeservice.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import org.example.financeservice.utils.Role;
 
-public record RegisterRequestDTO(String email, String name, String password, Role role) {
+public record RegisterRequestDTO(@NotBlank(message = "Email cannot be null.") String email, @NotBlank(message = "Name cannot be null.") String name, @NotBlank(message = "Password cannot be null") String password, Role role) {
 }
--- backend/finance-service/src/main/java/org/example/financeservice/dto/response/IncomeResponseDTO.java
+++ backend/finance-service/src/main/java/org/example/financeservice/dto/response/IncomeResponseDTO.java
@@ -1,16 +1,18 @@
 package org.example.financeservice.dto.response;
 
+import lombok.Data;
 import org.example.financeservice.model.Income;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Data
 public class IncomeResponseDTO{
-    UUID id;
-    BigDecimal amount;
-    LocalDate date;
-    String description;
+    private UUID id;
+    private BigDecimal amount;
+    private LocalDate date;
+    private String description;
 
     public IncomeResponseDTO(Income income) {
         this.id = income.getId();
