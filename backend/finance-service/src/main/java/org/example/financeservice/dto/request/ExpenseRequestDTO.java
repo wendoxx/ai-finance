88ad2d6676bebbb7d4313174cc611402600a006a
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-03-07 18:31:18 -0300

    chore: delete unused line

--- backend/finance-service/src/main/java/org/example/financeservice/dto/request/ExpenseRequestDTO.java
+++ backend/finance-service/src/main/java/org/example/financeservice/dto/request/ExpenseRequestDTO.java
@@ -1,11 +1,9 @@
 package org.example.financeservice.dto.request;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.UUID;
 
 public record ExpenseRequestDTO(UUID id, @NotNull BigDecimal amount, LocalDate date, String description) {
