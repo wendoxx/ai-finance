5faca2904e1d1101771f541bc274386528412249
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-03-18 14:53:24 -0300

    fix: correct type of attribute user

--- backend/finance-service/src/main/java/org/example/financeservice/dto/request/ExpenseRequestDTO.java
+++ backend/finance-service/src/main/java/org/example/financeservice/dto/request/ExpenseRequestDTO.java
@@ -1,10 +1,12 @@
 package org.example.financeservice.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Set;
 import java.util.UUID;
 
-public record ExpenseRequestDTO(UUID id, @NotNull BigDecimal amount, LocalDate date, String description) {
+public record ExpenseRequestDTO(UUID id, @NotNull BigDecimal amount, LocalDate date, String description, @JsonProperty("user") @NotNull UUID user) {
 }
