652cdc9244756cb5e3e2177eeb2ddbc4e8656211
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-03-18 17:03:02 -0300

    feat: add method to find total incomes and total expenses between dates

--- backend/finance-service/src/main/java/org/example/financeservice/repository/IncomeRepository.java
+++ backend/finance-service/src/main/java/org/example/financeservice/repository/IncomeRepository.java
@@ -2,10 +2,16 @@ package org.example.financeservice.repository;
 
 import org.example.financeservice.model.Income;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Repository
 public interface IncomeRepository extends JpaRepository<Income, UUID> {
+    @Query("SELECT SUM (i.amount) FROM Income i WHERE i.date BETWEEN :startDate AND :endDate")
+    BigDecimal findTotalIncomeBetweenDates(@Param("starDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 }
