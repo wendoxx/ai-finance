852aa941c975f6d8a9e7681fc91f3fc385a9ae33
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-03-18 14:50:54 -0300

    feat: update application.properties

--- backend/finance-service/src/main/resources/db/migration/V3__create_expense_table.sql
+++ backend/finance-service/src/main/resources/db/migration/V3__create_expense_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE expense (
     id UUID PRIMARY KEY,
     amount DECIMAL(10, 2) NOT NULL,
     date DATE NOT NULL,
-    description VARCHAR(255)
+    description VARCHAR(255),
     user_id UUID NOT NULL,
     CONSTRAINT fk_user_expense FOREIGN KEY (user_id) REFERENCES users(id)
 );
