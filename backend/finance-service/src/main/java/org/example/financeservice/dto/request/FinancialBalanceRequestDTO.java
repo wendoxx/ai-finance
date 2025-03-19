ddec19d21a619469320e28fec171fe8ab9bc59e6
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-03-19 16:50:16 -0300

    feat: add FinancialBalanceRequestDTO

--- backend/finance-service/src/main/java/org/example/financeservice/dto/request/FinancialBalanceRequestDTO.java
+++ backend/finance-service/src/main/java/org/example/financeservice/dto/request/FinancialBalanceRequestDTO.java
@@ -1,6 +1,7 @@
 package org.example.financeservice.dto.request;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
-public record FinancialBalanceRequestDTO(LocalDate startDate, LocalDate endDate) {
+public record FinancialBalanceRequestDTO(UUID user, LocalDate startDate, LocalDate endDate) {
 }
