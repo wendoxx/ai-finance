34f4235898ac39bea45558aa9eb566b3994958e9
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-03-20 18:34:53 -0300

    feat: add expense type enum

---
+++ backend/finance-service/src/main/java/org/example/financeservice/utils/ExpenseType.java
@@ -0,0 +1,12 @@
+package org.example.financeservice.utils;
+
+public enum ExpenseType {
+    FOOD,
+    TRANSPORT,
+    ENTERTAINMENT,
+    SHOPPING,
+    AESTHETIC,
+    HEALTH,
+    EDUCATION,
+    OTHER
+}
