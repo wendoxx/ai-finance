250ba7102ddddee5978d24b575fb7f4c36b79506
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-03-20 18:35:48 -0300

    feat: add logic to set expense's type

--- backend/finance-service/src/main/java/org/example/financeservice/service/ExpenseService.java
+++ backend/finance-service/src/main/java/org/example/financeservice/service/ExpenseService.java
@@ -6,6 +6,7 @@ import org.example.financeservice.model.Expense;
 import org.example.financeservice.model.User;
 import org.example.financeservice.repository.ExpenseRepository;
 import org.example.financeservice.repository.UserRepository;
+import org.example.financeservice.utils.ExpenseType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -35,6 +36,17 @@ public class ExpenseService {
 
         User user = userRepository.findById(expenseRequestDTO.user()).orElseThrow(() -> new RuntimeException("User not found"));
 
+        // it sets the expense type based on the string value
+        switch (expenseRequestDTO.type()) {
+            case "FOOD" -> expense.setType(ExpenseType.FOOD);
+            case "TRANSPORT" -> expense.setType(ExpenseType.TRANSPORT);
+            case "ENTERTAINMENT" -> expense.setType(ExpenseType.ENTERTAINMENT);
+            case "SHOPPING" -> expense.setType(ExpenseType.SHOPPING);
+            case "AESTHETIC" -> expense.setType(ExpenseType.AESTHETIC);
+            case "HEALTH" -> expense.setType(ExpenseType.HEALTH);
+            case "EDUCATION" -> expense.setType(ExpenseType.EDUCATION);
+            case "OTHER" -> expense.setType(ExpenseType.OTHER);
+        }
         expense.setAmount(expenseRequestDTO.amount());
         expense.setDate(expenseRequestDTO.date());
         expense.setDescription(expenseRequestDTO.description());
