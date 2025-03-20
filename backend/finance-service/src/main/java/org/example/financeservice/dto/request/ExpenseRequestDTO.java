9a6f6484ebf86a012eb92aaf65ff26807e5e775a
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-03-20 18:34:01 -0300

    feat: add attribute type to expense dtos

--- backend/finance-service/src/main/java/org/example/financeservice/dto/request/ExpenseRequestDTO.java
+++ backend/finance-service/src/main/java/org/example/financeservice/dto/request/ExpenseRequestDTO.java
@@ -1,11 +1,12 @@
 package org.example.financeservice.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record ExpenseRequestDTO(UUID id, @NotNull BigDecimal amount, LocalDate date, String description, @JsonProperty("user") @NotNull UUID user) {
+public record ExpenseRequestDTO(UUID id, @NotNull BigDecimal amount, LocalDate date, String description, @JsonProperty("user") @NotNull UUID user, @NotBlank String type) {
 }
