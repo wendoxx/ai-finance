809bfd3c336230e3c8aa56bbeee4cfc2c45ce8dd
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-04-14 19:20:18 -0300

    feat: add login component to angular project

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: "",
+  component: LoginComponent
+}, {
+  path: "home",
+  component: LoginComponent
+}
+];
